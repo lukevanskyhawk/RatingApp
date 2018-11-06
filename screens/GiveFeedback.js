a107cd5e4d29ce9bab568083764f0f036a7b4466
Brad Van Skyhawk <brad@vanskyhawk.com>
2018-11-06 09:23:59 -0700

    Add rating component

--- screens/GiveFeedback.js
+++ screens/GiveFeedback.js
@@ -8,24 +8,44 @@ import {
   View,
 } from 'react-native';
 import { TextInput } from 'react-native-gesture-handler';
+import StarRating from 'react-native-star-rating';
 
 export default class GiveFeedbackScreen extends React.Component {
   static navigationOptions = {
     title: 'Give Feedback',
   };
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      starCount: 0
+    };
+  }
+
+  onStarRatingPress(rating) {
+    this.setState({
+      starCount: rating
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text>Email of person you are rating</Text>
-        <TextInput style={styles.emailInput} placeholder="Email" />
+        <TextInput style={styles.emailInput} keyboardType="email-address" placeholder="Email" />
 
-        <Text>This is where the stars will be</Text>
+        <Text>Rating</Text>
+        <StarRating
+          containerStyle={styles.ratingInput}
+          disabled={false}
+          maxStars={5}
+          fullStarColor={'#fba634'}
+          rating={this.state.starCount}
+          selectedStar={(rating) => this.onStarRatingPress(rating)}
+        />
 
         <Text>Comments</Text>
         <TextInput style={styles.commentInput} placeholder="Comments" multiline={true} />
-
-
 
         <TouchableOpacity
           style={styles.submitButton}
@@ -53,21 +73,30 @@ const styles = StyleSheet.create({
     height: 40,
     margin: 15,
     padding: 3,
+    borderRadius: 3
+  },
+  ratingInput: {
+    height: 50,
+    margin: 15,
+    padding: 3
   },
   commentInput: {
     borderWidth: 1,
     height: 200,
     margin: 15,
     padding: 3,
-
+    borderRadius: 3
   },
   submitButton: {
-    backgroundColor: '#7a42f4',
+    backgroundColor: '#339afa',
     padding: 10,
     margin: 15,
-    height: 40,
+    height: 60,
+    borderRadius: 5
   },
   submitButtonText: {
-    color: 'white'
+    color: 'white',
+    fontSize: 30,
+    textAlign: 'center'
   }
 });
